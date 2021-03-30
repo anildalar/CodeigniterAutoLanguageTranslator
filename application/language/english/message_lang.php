27f298daa34a5f338215ac4a3e7a3eb02d4e4608
anildalar <dollorinfotech@gmail.com>
2021-03-30 13:26:02 +0530

    Auto Translation

---
+++ application/language/english/message_lang.php
@@ -0,0 +1,130 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+	$lang['separated_by_comma'] = "" ;
+	$lang['no_recordes_available'] = "No recordes available" ;
+	$lang['request_not_allow'] = "You'r request is not allow, please try one more time." ;
+	$lang['select_option'] = "Please select any of optoin" ;
+	$lang['pack_store_success'] = "Pack details succeffully stored." ;
+	$lang['pack_update_success'] = "Pack details succeffully updated." ;
+	$lang['something-wrong'] = "Something is wrong,Please try again." ;
+	$lang['product_dairect_buy'] = "Do you really want to buy product." ;
+	$lang['product_in_card_avialble'] = "You'r selected product allready available in your card, Please try to buy product.";
+	$lang['product_add_card'] = "You'r selected product succeffully add in card, Do you really buy know product?" ;
+	$lang['cart_empty_msg'] = "Cart is empty" ;
+	$lang['check_out_with']="Check out with";
+	$lang['card_confime_txt']="Go to cart";
+	$lang['card_cancel_txt']="Continue purchasing";
+	$lang['user_confirm']="Do you want to";
+	$lang['empty_cart_sms']=" Empty your cart?";
+	$lang['empty_cart_sms_return']="You'r cart succeffully empty. Do you want to go purchasing new productes.";
+	$lang['sucess_update']='Your %1$s succeffully updated.'; // exmple sprintf($this->lang->line('sucess_update'), 'proudct')
+	$lang['sucess_update_record']='Record succeffully updated.'; // exmple sprintf($this->lang->line('sucess_update'), 'proudct')
+	$lang['sucess_stored']='Your %1$s record succeffully stored.'; // exmple sprintf($this->lang->line('sucess_stored'), 'proudct')
+	$lang['sucess_removed']='Your %1$s record succeffully removed.'; // exmple sprintf($this->lang->line('sucess_removed'), 'proudct')
+	$lang['something_wrong']="Something is wrong, Please try one more time";
+	$lang['something_missing']="Something is missing in request, Please try one more time";
+	$lang['required_fields_not_match']="Required fiedls not matched.";
+	$lang['no_found_normal']='No %1$s found';
+	$lang['no_match']='No matching records found';
+	$lang['do_want_message_blacklist']='Do you want to blacklist this channel ?';
+	$lang['do_want_message_whitelist']='Do you want to whitelist this channel ?';
+	$lang['pschanneltype']="Please select channel type";
+	$lang['whitelist_placeholder']="Enter whitelist ips sparated  by comma.";
+	$lang['plsenteropt']="Please enter port";
+	$lang['port_notempty_message']="port is use by another services";
+	$lang['tech_error']='Technical Error : %1$s ';
+	$lang['record_exists']="Record already exists";
+	$lang['inactive_language']="Inactive this Language";
+	$lang['active_language']="Active this language";
+	$lang['are_you_sure']="Are you sure ?";
+	$lang['language_change_message']="To change Language status";
+	$lang['lang_dlt_msg']="To delete this Language.";
+	$lang['lang_romove']="Language succeffully removed.";
+	$lang['wrong_carrier_type']="Please select valid Carrier Type.";
+	$lang['no_carrier_found']="No Carrier Found.";
+	$lang['show_entries']='Show %1$s entries';
+	$lang['no_data_in_tbl']="No data available in table";
+	$lang['current_domain_msg']="usr only login with current Domain.";
+	$lang['plsecrrier']="Please select Carrier.";
+	$lang['plz_sect_shedati']="Please select shedule Date and time.";
+	/*  <?php echo $this->lang->line('');?>   */
+
+/*
+	
+*/
+
+	$lang['accUpButSNot']="Account details updated, try Switch Manually Update.";
+	$lang['accAswiUpdate']="Account and Switch details updated."; 
+	$lang['creditUpdateAcSW']="Account Balance succeffully updated";
+	$lang['creditUpdateAcSWN']="Account Balance updated, try Switch Manually Update.";
+	$lang['creditUpdate']="Account Balance successfully updated.";
+	$lang['prowithSwsuccess']="Product & Switch record successfully updated.";
+	$lang['proYSwNsuccess']="Product record successfully updated. try Switch Manually Update.";
+	$lang['proYSwNerror']="Product record successfully updated.Switch need Manually update.";
+	$lang['chawithSwsuccess']="Channel & Switch record successfully updated.";
+	$lang['chanYSwNsuccess']="Channel record successfully updated. try Switch Manually Update.";
+	$lang['cheYSwNerror']="Channel record successfully updated.Switch need Manually update.";
+	$lang['delete_sms_alert']="Once deleted, we not be able to send this message!";
+	$lang['shedule_sms_remove']="Shedule message successfully removed in system";
+
+
+
+	/****************************************************************************************************************************/
+	$lang['invalid_file_type']="Invalid File type, Please select correct file type.";
+	$lang['select_only_csv']="Invalid file, please select only CSV file.";
+	$lang['select_file']="Please select file.";
+	$lang['erroron_file_upload']="Error on file upload, please try again.";
+	$lang['select_valid_file']="Please select valid file.";
+	$lang['select_linecorrect_splitter']="Please select line correct splitter.";
+	$lang['select_rowcorrect_splitter']="Please select row correct splitter.";
+	$lang['stored_empty_record']="We can't stored empty record file, Please try with correct file.";
+	$lang['number_blacklisted_globle']="Number allready Blacklisted globle system.";
+	$lang['blacklist_contact_stored']="Blacklist Contact successfully stored.";
+	$lang['record_updated']="Record successfully Updated.";
+	$lang['successfully_process']="You'r file successfully process.";
+	$lang['process_not_allow']="Your process is not allow.";
+	$lang['upload_csvfile']="Please upload csv file.";
+	$lang['please_upload_file']="Please upload file.";
+	$lang['filenot_process']="Sorry you'r send message file not process, Please try again.";
+	$lang['contact_successfully_removed']="Your contact successfully removed.";
+	$lang['contact_successfully_updated']="Your contact successfully updated.";
+	/*  <?php echo $this->lang->line('');?>   */
+	$lang['incorrect_routeid']="Incorrect Route id.";
+	$lang['select_route_first']="Please select Route first.";
+	$lang['successfully_approved_senderid']="Sender ID Successfully Approved.";
+	$lang['senderid_update']="Sender ID Successfully Update.";
+
+
+
+	//////Invalid Route
+	$lang['invalid_route']="Invalid Route.";
+	$lang['invalid_routing']="Invalid Routing.";
+	$lang['insufficient_balance']="Insufficient Balance";
+	$lang['invalid_destinationAddr_invalid_routing']="Invalid destinationAddr And Invalid Routing";
+	$lang['invalid_apiKey']="Invalid apiKey";
+	$lang['empty_destinationAddr']="Empty destinationAddr";
+
+
+	//Selected language
+	$lang['selected_language_not_mapping']="Selected Language not mapping with system.";
+	$lang['selected_language_system']="Selected Language not match with system.";
+
+	// Username and password
+	$lang['invalid_username_password']="Invalid Username or Password!";
+	$lang['account_not_active']="Your Account is Not Active.";
+
+	//Image File
+	$lang['image_save']="Image file successfully save.";
+	$lang['invalid_type']="Invalid type";
+	$lang['invalid_operation']="Invalid Operation";
+	$lang['choose_actual_image']="Please choose actual image file and try again.";
+	$lang['choose_less_then']="Please choose less then 2MB file and try again";
+
+	//Api Key
+	$lang['api_key_updated']="Api Key Successfully Updated.";
+	///////////////////***********************************//////////////////////////
+	$lang['not_date_in_file']="No Date available in file for import.";
+	$lang['select_xlsx_file']="Please select .xlsx file.";
+
+
+?>
